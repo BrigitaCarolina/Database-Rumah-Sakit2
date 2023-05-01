4c0821d6ee5b70b888f78138d3c8d11ecb8f4f9d
ammarasyad <ammarasyad@gmail.com>
2023-05-01 23:13:09 +0700

    Add departemenTenagaMedis and lisensiTenagaMedis

--- departemenTenagaMedis.py
+++ departemenTenagaMedis.py
@@ -1,5 +1,5 @@
 import random
-def create_departemen_tenaga_medis(connection):
+def create_dept_tenaga_medis(connection):
     create_table_query = """
     CREATE TABLE IF NOT EXISTS DepartemenTenagaMedis (
         nomor_lisensi       VARCHAR(25) NOT NULL PRIMARY KEY,
@@ -13,6 +13,14 @@ def create_departemen_tenaga_medis(connection):
         cursor.execute(create_table_query)
 
 
-def input_data_tenaga_medis(x, connection, fake):
+def input_data_dept_tenaga_medis(connection):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT ")
+        cursor.execute("SELECT nomor_lisensi FROM LisensiTenagaMedis")
+        data_nomor = cursor.fetchall()
+
+        cursor.execute("SELECT id FROM Departemen")
+        data_id_dept = cursor.fetchall()
+
+        for i in data_id_dept:
+            cursor.execute(f"INSERT INTO DepartemenTenagaMedis (nomor_lisensi, id_departemen) VALUES ('{data_nomor[i]}', {data_id_dept[i]['id']})")
+
