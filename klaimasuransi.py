73e42eec3f7d2dacdf73619c019627ba998e4076
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-05-03 03:12:16 +0700

    feat: add jumlah klaim

--- klaimasuransi.py
+++ klaimasuransi.py
@@ -7,6 +7,7 @@ def create_klaim_asuransi(connection):
         status              varchar(255) NOT NULL,
         id_perusahaan       int NOT NULL,
         id_medical_records  int NOT NULL,
+        jumlah              int NOT NULL,
         PRIMARY KEY (id),
         FOREIGN KEY (id_perusahaan) REFERENCES PerusahaanAsuransi(id),
         FOREIGN KEY (id_medical_records) REFERENCES MedicalRecord(id)
@@ -35,7 +36,13 @@ def insert_klaim_asuransi(x, connection, fake):
             if random_id in [d['id'] for d in id_medical_records_list]:
                 id_medical_records_list.remove({'id': random_id})
                 success = True
-        query = "INSERT INTO KlaimAsuransi VALUES (%s, %s, %s, %s, %s)"
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT biaya from detailrecords join medicalrecord on detailrecords.id_pasien = medicalrecord.id_pasien and detailrecords.id_kerja_sama = medicalrecord.id_kerja_sama where medicalrecord.id = %s", (random_id))
+                    jumlah = cursor.fetchone()['biaya']
+                if jumlah == 0:
+                    success = False
+        jumlah = random.randint(1, jumlah)
+        query = "INSERT INTO KlaimAsuransi VALUES (%s, %s, %s, %s, %s, %s)"
         with connection.cursor() as cursor:
-            cursor.execute(query, (id, tanggalKlaim, status, id_perusahaan, random_id))
+            cursor.execute(query, (id, tanggalKlaim, status, id_perusahaan, random_id, jumlah))
         connection.commit()
